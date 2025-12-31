23dc928dc556c27da3138bfab46f893382325f12
Saif Smran <23303079@iubat.edu>
2025-12-31 18:06:28 +0600

    Connect to mongo atlas

--- src/index.js
+++ src/index.js
@@ -1,1 +1,30 @@
+// require("dotenv").config({path: "/.env"});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
 
+dotenv.config({ path: "./.env" });
+
+
+connectDB();
+
+/*
+import express from "express";
+const app = express();
+(async () => {
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log("DB connected successfully");
+        app.on("errror", (error) => {
+            console.log("ERRR: ", error);
+            throw error
+
+        });
+        app.listen(process.env.PORT, () => {
+            console.log(`App is listening on port ${process.env.PORT}`);
+        });
+    } catch (error) {
+        console.error("Error in DB connection", error);
+        throw error;
+    }
+})()
+*/
